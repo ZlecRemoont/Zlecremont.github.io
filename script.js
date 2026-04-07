99f52fa38412846adcbabdc19b7199d73f6b8138
ZlecRemoont <soko8399999@gmail.com>
2026-04-07 13:20:40 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
+let jobs = [];
 
+function addJob() {
+  const title = document.getElementById("title").value;
+  const city = document.getElementById("city").value;
+
+  if (!title || !city) {
+    alert("Uzupełnij pola!");
+    return;
+  }
+
+  const job = { title, city };
+  jobs.push(job);
+
+  renderJobs();
+}
+
+function renderJobs() {
+  const list = document.getElementById("jobsList");
+  list.innerHTML = "";
+
+  jobs.forEach(j => {
+    const div = document.createElement("div");
+    div.className = "job";
+    div.innerHTML = `<strong>${j.title}</strong><br>${j.city}`;
+    list.appendChild(div);
+  });
+}
+
+function showLogin() {
+  alert("Tu będzie logowanie (backend)");
+}
